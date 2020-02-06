0a6325559fbfbe27516926782af83cb47df4a80e
Ivan Novitskiy <40142837+RedClusive@users.noreply.github.com>
2020-02-06 16:47:52 +0300

    Add files via upload

--- database/database.go
+++ database/database.go
@@ -24,16 +24,19 @@ const (
 func ConnectToDB() *sql.DB  {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		fmt.Println("Can't open data base:")
-		log.Fatal(err)
+	for {
+		db, err := sql.Open("postgres", psqlInfo)
+		if err != nil {
+			log.Println("Can't open data base:")
+			log.Println(err)
+		} else {
+			err = db.Ping()
+			if err != nil {
+				log.Fatal(err)
+			}
+			return db
+		}
 	}
-	err = db.Ping()
-	if err != nil {
-		log.Fatal(err)
-	}
-	return db
 }
 
 func DBClose(db *sql.DB) {
