96d23a9dc3b0906f9d5bc8ca3523c37f877e1952
Ivan Novitskiy <40142837+RedClusive@users.noreply.github.com>
2020-02-09 21:10:35 +0300

    Add files via upload

--- database/configEnv.go
+++ database/configEnv.go
@@ -24,5 +24,6 @@ func SetUpConfig() {
 	host = getEnv("HOST", host)
 	port= getIntEnv("PORT", port)
 	user = getEnv("USER", user)
-	password = getEnv("DBNAME", password)
+	password = getEnv("PASSWORD", password)
+	dbname = getEnv("DBNAME", dbname)
 }
